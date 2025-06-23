72ac287a61bd203a6feb780da4324b7a614c5e7e
abhinusekharlal <abhinuyouramigo123@gmail.com>
2025-06-23 23:16:46 +0530

    Remove unused templates for home and marketplace pages, updating the project structure for better organization. Database updated to reflect these changes.

--- akrionline/marketplace/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'marketplace'
-
-urlpatterns = [
-    # Marketplace URLs will be added here
-    path('', views.marketplace_home, name='home'),
-]
--- akrionline/marketplace/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def marketplace_home(request):
-    """Marketplace home view - placeholder"""
-    return render(request, 'marketplace/home.html')
